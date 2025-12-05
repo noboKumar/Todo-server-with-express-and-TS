2f710d723132423e9d95559793069fa2caf0acc3
noboKumar <nobokumar227@gmail.com>
2025-12-05 17:52:35 +0600

    add get user dynamic data with id

--- src/server.ts
+++ src/server.ts
@@ -88,7 +88,31 @@ app.get("/users", async (req: Request, res: Response) => {
   }
 });
 
-
+// get user :id
+app.get("/users/:id", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM users WHERE id = $1`, [
+      req.params.id,
+    ]);
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User Not Found",
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "User found",
+        data: result.rows[0],
+      });
+    }
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
